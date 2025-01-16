18655f5661c9974b6293bd1b4eb8dad20b1ec141
Sonya P <133888064+maybemountains@users.noreply.github.com>
2025-01-16 15:28:25 +0100

    feat: add player cellvalue verification

--- app/src/main/java/frog/frogtasticfour/tictactwo/data/enums/CellValue.java
+++ app/src/main/java/frog/frogtasticfour/tictactwo/data/enums/CellValue.java
@@ -1,9 +1,15 @@
 package frog.frogtasticfour.tictactwo.data.enums;
 
+import java.util.EnumSet;
+
 public enum CellValue {
     Empty,
     X,
     O,
     Tie,
-    Calculate
+    Calculate;
+
+    public boolean isPlayer() {
+        return EnumSet.of(O, X).contains(this);
+    }
 }
