fb99a5ff4e58c14cc085de60b0a6241b0e20726b
Sonya P <133888064+maybemountains@users.noreply.github.com>
2025-01-16 15:27:08 +0100

    feat: add basic game structure

---
+++ app/src/main/java/frog/frogtasticfour/tictactwo/data/Game.java
@@ -0,0 +1,24 @@
+package frog.frogtasticfour.tictactwo.data;
+
+import frog.frogtasticfour.tictactwo.data.enums.CellValue;
+
+public class Game {
+    private final Board _board;
+
+    private CellValue _turn;
+    public Board getBoard() {
+        return _board;
+    }
+    public boolean hasWinner() {
+        return _board.getValue() != CellValue.Empty;
+    }
+
+    public CellValue getWinner() {
+        return _board.getValue();
+    }
+
+    public CellValue getTurn() {
+        return _turn;
+    }
+
+}
