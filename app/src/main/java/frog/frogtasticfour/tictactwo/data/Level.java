85a889f40c5c5553aaede819e2df6608250ba022
Kan <38980663+KanTheAstronaut@users.noreply.github.com>
2025-01-16 15:40:43 +0100

    feat: add base level list structure

---
+++ app/src/main/java/frog/frogtasticfour/tictactwo/data/Level.java
@@ -0,0 +1,59 @@
+package frog.frogtasticfour.tictactwo.data;
+
+public class Level {
+    private Level _parent;
+    private Level _child;
+    private final Board _board;
+    private int _depth;
+
+    public Level(Board board, int initialDepth) {
+        _board = board;
+        _depth = initialDepth;
+    }
+
+    public int getDepth() {
+        return _depth;
+    }
+
+    public Level setParent(Level parent) {
+        _parent = parent;
+        return this;
+    }
+
+    public Level setChild(Level child) {
+        _child = child;
+        return this;
+    }
+
+    public Level getParent() {
+        return _parent;
+    }
+
+    public Board getBoard() {
+        return _board;
+    }
+
+    public boolean hasParent() {
+        return _parent != null;
+    }
+
+    public boolean hasChild() {
+        return _child != null;
+    }
+
+    public Level getFirstParent() {
+        Level level = this;
+        while (level.hasParent()) {
+            level = level._parent;
+        }
+        return level;
+    }
+
+    public Level getLastChild() {
+        Level level = this;
+        while (level.hasChild()) {
+            level = level._child;
+        }
+        return level;
+    }
+}
