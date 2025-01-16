2a3e14ef54eef43404da51815f0581319cc1533f
Kan <38980663+KanTheAstronaut@users.noreply.github.com>
2025-01-16 14:44:11 +0100

    feat: use matrix creator to create board

--- app/src/main/java/frog/frogtasticfour/tictactwo/data/Board.java
+++ app/src/main/java/frog/frogtasticfour/tictactwo/data/Board.java
@@ -10,6 +10,24 @@ public class Board implements IGridable{
         _grid = grid;
     }
 
+    private static final MatrixCreator<Cell> _cellCreator = new MatrixCreator<>(Cell::new, Cell.class);
+
+    public static Board CreateBoard() {
+        return CreateBoard(2, 3);
+    }
+
+    public static Board CreateBoard(int levels) {
+        return CreateBoard(levels, 3);
+    }
+
+    public static Board CreateBoard(int levels, int size) {
+        var interiorLevel = levels - 1;
+        var boardCreator = new MatrixCreator<>(() -> CreateBoard(interiorLevel, size), Board.class);
+        if (interiorLevel == 0) //its cellable
+            return new Board(_cellCreator.Generate(size, size));
+        else
+            return new Board(boardCreator.Generate(size, size));
+    }
 
     @Override
     public CellValue getValue() {
