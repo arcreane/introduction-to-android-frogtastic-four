2812f53eb3a9d2625d4ad61d4b92b4575d8631d1
LeonoreNdebele <127037813+LeonoreNdebele@users.noreply.github.com>
2025-01-16 15:04:37 +0100

    feat: add cell value validation

--- app/src/main/java/frog/frogtasticfour/tictactwo/data/Cell.java
+++ app/src/main/java/frog/frogtasticfour/tictactwo/data/Cell.java
@@ -1,6 +1,7 @@
 package frog.frogtasticfour.tictactwo.data;
 
 import frog.frogtasticfour.tictactwo.data.enums.CellValue;
+import frog.frogtasticfour.tictactwo.exceptions.CellValueException;
 
 public class Cell implements IGridable {
     private CellValue _value = CellValue.Empty;
@@ -11,6 +12,12 @@ public class Cell implements IGridable {
 
     @Override
     public void setValue(CellValue value) {
+        if (_value != CellValue.Empty)
+            throw new CellValueException("This cell was already set, you cannot change the value");
+        if (value == CellValue.Calculate)
+            throw new CellValueException("You cannot calculate the winner of a cell");
+        if (value == CellValue.Tie)
+            throw new CellValueException("Cell can't tie");
         _value = value;
     }
 }
